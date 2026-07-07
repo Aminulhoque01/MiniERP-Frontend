f68371a772c1353403e95e564c0793cee390a113
Aminulhoque01 <aminuhoque53@gmail.com>
2026-07-07 23:29:49 +0600

    build

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: import.meta.env.VITE_API_URL,
+  baseUrl: `https://mini-erp-backend-delta.vercel.app/api`,
 
   prepareHeaders: (headers) => {
     const token = localStorage.getItem("accessToken");
